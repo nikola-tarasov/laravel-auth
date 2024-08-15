11c3e3f910521a05afbed163d1bf7ecae6f523dd
Nikolazzzer <nikola-tarasov@mail.ru>
2024-08-15 13:20:30 +0300

    Добавлены страницы видов для login, register в отдельную папку user

---
+++ resources/views/user/login.blade.php
@@ -0,0 +1,12 @@
+@extends('layout\main')
+
+
+
+
+@section('title', 'login')
+
+
+@section('content')
+    <h1>Login page</h1>
+@endsection
+
---
+++ resources/views/user/register.blade.php
@@ -0,0 +1,11 @@
+@extends('layout\main')
+
+
+
+
+@section('title', 'register')
+
+
+@section('content')
+    <h1>Register page</h1>
+@endsection
