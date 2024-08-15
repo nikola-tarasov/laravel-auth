2f260685ceb16f9542ef8e46568b63facb08ecb4
Nikolazzzer <nikola-tarasov@mail.ru>
2024-08-15 13:26:00 +0300

    Добавлен phpBlock для путей

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 
+/**
+ *
+ * функция по методу get возвращает шаблон "welcom"
+ *
+ * @returns View
+ *
+ */
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
+/**
+ * вывод путей по контроллерам
+ */
+Route::get('login', [\App\Http\Controllers\UserController::class, 'create'])->name('login');
+
+Route::post('register', [\App\Http\Controllers\UserController::class, 'store'])->name('store.register');
+
+Route::get('register', [\App\Http\Controllers\UserController::class, 'register'])->name('register');
+
+
+
