bee7359e89c046b9145916aee08419a720eae150
Nikolazzzer <nikola-tarasov@mail.ru>
2024-08-15 18:49:24 +0300

    исправлена верстка,добавлен вывод валидоционных ошибок

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -38,6 +38,17 @@
 
 <main class="main mt-3">
     <div class="container">
+
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+
         @yield('content')
     </div>
 </main>
