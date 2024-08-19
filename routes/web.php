4724cc97b3999aa628e8990f02fd922d00ce7038
Nikolazzzer <nikola-tarasov@mail.ru>
2024-08-19 12:16:42 +0300

    добавлен маршруты в группу middleware Изменен метод отправки повторного письма с post на get

--- routes/web.php
+++ routes/web.php
@@ -15,39 +15,60 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-/**
- * вывод путей по контроллерам
- */
-Route::get('dashboard', [\App\Http\Controllers\UserController::class, 'dashboard'])->name('dashboard');
 
-Route::get('login', [\App\Http\Controllers\UserController::class, 'create'])->name('login');
-
-Route::post('register', [\App\Http\Controllers\UserController::class, 'store'])->name('store.register');
-
-Route::get('register', [\App\Http\Controllers\UserController::class, 'register'])->name('register');
+Route::middleware(['auth', 'verified'])->group(function (){
+    Route::get('dashboard', [\App\Http\Controllers\UserController::class, 'dashboard'])->name('dashboard');
+});
 
 
-//маршрут обработки отправки на подтверждения
-Route::get('email-verify', function () {
-    return view('user.verify-email');
-})->middleware('auth')->name('verification.notice');
+Route::middleware('guest')->group(function (){
 
-//обработчик емайла
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
+    /**
+     * вывод путей по контроллерам
+     */
+    Route::get('login', [\App\Http\Controllers\UserController::class, 'create'])->name('login');
 
-    return redirect()->route('dashboard');
-})->middleware(['auth', 'signed'])->name('verification.verify');
+    Route::post('register', [\App\Http\Controllers\UserController::class, 'store'])->name('store.register');
+
+    Route::get('register', [\App\Http\Controllers\UserController::class, 'register'])->name('register');
+
+});
 
 
-//если пользователю не пришло ссылка на почту
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
 
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:3,1'])->name('verification.send');
+Route::middleware('auth')->group(function (){
 
-//маршрут разлогирования
-Route::get('logout', [\App\Http\Controllers\UserController::class, 'logout'])->name('logout');
+    //маршрут обработки отправки на подтверждения
+    Route::get('email-verify', function () {
+        return view('user.verify-email');
+    })->name('verification.notice');
+
+    //обработчик емайла
+    Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+        $request->fulfill();
+
+        return redirect()->route('dashboard');
+    })->middleware(['signed'])->name('verification.verify');
+
+    //если пользователю не пришло ссылка на почту
+    Route::get('/email/verification-notification', function (Request $request) {
+        $request->user()->sendEmailVerificationNotification();
+
+        return back()->with('message', 'Verification link sent!');
+    })->middleware(['throttle:3,1'])->name('verification.send');
+
+    //маршрут разлогирования
+    Route::get('logout', [\App\Http\Controllers\UserController::class, 'logout'])->name('logout');
+
+});
+
+
+
+
+
+
+
+
+
 
 
