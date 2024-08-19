7fd49fa4eefe9f98b7016ca20061aa8c0a5de318
Nikolazzzer <nikola-tarasov@mail.ru>
2024-08-19 14:03:22 +0300

    добавления вида для аутентификации

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -7,13 +7,24 @@
 
 
 @section('content')
-    <h1>Login page</h1> <br>
+    <h1>Login</h1> <br>
+    <div class="row justify-content-center">
+        <div class="col-4 ">
+            <form action="{{route('authenticate')}}" method="post" >
+                @csrf
 
-{{--    <ul>--}}
-{{--        <li>{{$name}}</li>--}}
-{{--        <li>{{$email}}</li>--}}
-{{--    </ul>--}}
-
+                <div class="mb-3">
+                    <label for="name" class="form-label">Login</label>
+                    <input name="name" type="text" class="form-control" id="name">
+                </div>
+                <div class="mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input name="password" type="password"  class="form-control" id="password">
+                </div>
+                <button  type="submit" class="btn btn-primary">Войти</button>
+            </form>
+        </div>
+    </div>
 
 @endsection
 
